a1c207b655f5285e4c03c848ac4dea9ef299d4a8
luismomm2110 <luismomm@DESKTOP-2A9EV4J.localdomain>
2021-06-18 20:09:27 -0300

    Sent email ok

--- SMTP/mailclient.py
+++ SMTP/mailclient.py
@@ -53,58 +53,48 @@ print(clientSocket.recv(1024).decode())
 clientSocket.send((base64.b64encode((password).encode())) + ('\r\n').encode())
 print(clientSocket.recv(1024).decode())
 
-# Fill in start
 mailFrom = "MAIL FROM: <{}> \r\n".format(userName)
 print(mailFrom)
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 recv2 = recv2.decode()
 print("Message after MAIL FROM: ", recv2)
-# Fill in end
 
 # Send RCPT TO command and print server response.
-# Fill in start
-rcptTo = "RCPT TO: <xxxxxx>\r\n"
+
+destinatary = input("Send to: ")
+rcptTo = "RCPT TO: <{}>\r\n".format(destinatary)
 clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024)
 recv3 = recv3.decode()
 print("Message after RCPT TO: ", recv3)
 
-# Fill in end
 
 # Send DATA command and print server response.
-# Fill in start
-data = "DATA: <xxxxxx>\r\n"
+
+data = "DATA" + " \r\n"
 clientSocket.send(data.encode())
 recv4 = clientSocket.recv(1024)
 recv4 = recv4.decode()
 print("Message after DATA: ", recv4)
 
-# Fill in end
+# Send message data
 
-# Send message data.
-
-# Fill in start
-body = "Hello, I'm using my own mail server for this \r\n"
+#Send subject
+clientSocket.send(("Subject: Test with github!" + "\r\n").encode())
+#send body
+body = "Hello, I'm using my own mail client for this. You can check at github.com/luismomm2110/network-kurose \r\n"
 clientSocket.send(body.encode())
-
-# Fill in end
-
-
 # Message ends with a single period.
 
-# Fill in start
 period = ".\r\n"
 clientSocket.send(period.encode())
 
-# Fill in end
-
 # Send QUIT command and get server response.
-# Fill in start
+
 quit = "QUIT\r\n"
 clientSocket.send(quit.encode())
 recv6 = clientSocket.recv(1024)
 recv6 = recv6.decode()
-print(recv6)
+print("Message after quit: ", recv6)
 clientSocket.close()
-#Fill in end
